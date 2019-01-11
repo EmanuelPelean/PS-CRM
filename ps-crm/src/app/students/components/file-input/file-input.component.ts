f266d0640bbb799393427901cfdeab75cc9ea891
Emanuel Pelean <manu.pelean@gmail.com>
2019-01-10 20:39:18 -0500

    CRL-2004: add file-input component

---
+++ ps-crm/src/app/students/components/file-input/file-input.component.ts
@@ -0,0 +1,11 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-file-input',
+  templateUrl: 'file-input.component.html',
+  styleUrls: ['file-input.component.less']
+})
+
+export class FileInputComponent {
+
+}

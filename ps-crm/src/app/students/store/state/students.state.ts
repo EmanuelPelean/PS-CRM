679d5c72ae871061ee2d21689fa84adec6f690df
Emanuel Pelean <manu.pelean@gmail.com>
2019-01-13 03:02:31 -0500

    CRL-2007: create students store selectors

--- ps-crm/src/app/students/store/state/students.state.ts
+++ ps-crm/src/app/students/store/state/students.state.ts
@@ -7,7 +7,22 @@ export interface StudentsState {
 }
 
 export const initialState: StudentsState = {
-  data: [],
+  data: [
+    {
+      id: 1,
+      date_submitted: '01/06/2019',
+      ip_address: '1.234.876',
+      opt_in: 'unknown',
+      name_first: 'Tom',
+      name_last: 'Hanks',
+      address: '123 Main St',
+      phone: '910-999-9989',
+      email: 'tom@me.com',
+      program_type: 'Segment 1',
+      course_time: 'Mon 8pm',
+      additional_comments: 'excited',
+      }
+  ],
   loaded: false,
   loading: false,
 };

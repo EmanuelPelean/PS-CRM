e1da65b2745c2b35b96ce59c4ab5cf82adeac484
Emanuel Pelean <manu.pelean@gmail.com>
2019-01-13 02:03:19 -0500

    CRL-2007: add students load actions

---
+++ ps-crm/src/app/students/models/student.model.ts
@@ -0,0 +1,16 @@
+
+export interface Student {
+  id?: number;
+  date__submitted?: string;
+  ip__address?: string;
+  opt__in?: string;
+  name__first?: string;
+  name__last?: string;
+  address?: string;
+  phone?: string;
+  email?: string;
+  program__type?: string;
+  course__time?: string;
+  additional__comments?: string;
+  duplicate__course__time?: string;
+}

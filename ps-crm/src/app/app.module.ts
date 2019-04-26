c71ee55637fb76f7235d26f63d0d722f6f8f52cd
Emanuel Pelean <manu.pelean@gmail.com>
2019-04-25 23:40:10 -0400

    initial firestore setup

--- ps-crm/src/app/app.module.ts
+++ ps-crm/src/app/app.module.ts
@@ -1,19 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { StoreModule, MetaReducer } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
-
-// not used in production
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { storeFreeze } from 'ngrx-store-freeze';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatListModule } from '@angular/material';
 import { AuthModule } from './auth/auth.module';
-import {SharedModule} from './shared/shared.module';
+import { SharedModule } from './shared/shared.module';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { config } from './auth/firebase-config';
 
 // this would be done dynamically with webpack for builds
 const environment = {
@@ -33,6 +32,8 @@ export const metaReducers: MetaReducer<any>[] = !environment.production
     BrowserAnimationsModule,
     AuthModule,
     SharedModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule,
     StoreModule.forRoot({}, { metaReducers }),
     EffectsModule.forRoot([]),
     environment.development ? StoreDevtoolsModule.instrument() : []

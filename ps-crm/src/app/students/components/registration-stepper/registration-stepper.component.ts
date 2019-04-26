c0ea43767199a7a1b4e3bb75f8bddcd07b72b3d6
Emanuel Pelean <manu.pelean@gmail.com>
2019-04-25 23:27:17 -0400

    add second step properties to component

--- ps-crm/src/app/students/components/registration-stepper/registration-stepper.component.ts
+++ ps-crm/src/app/students/components/registration-stepper/registration-stepper.component.ts
@@ -10,6 +10,7 @@ export class RegistrationStepperComponent implements OnInit {
   isLinear = false;
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
+  thirdFormGroup: FormGroup;
 
   constructor(private _formBuilder: FormBuilder) {}
 
@@ -24,10 +25,36 @@ export class RegistrationStepperComponent implements OnInit {
       address_state: ['', Validators.required],
       address_zip: ['', Validators.required],
       phone_cell: ['', Validators.required],
-      email: ['', Validators.required]
+      email: ['', Validators.required],
+      sex: ['', Validators.required]
     });
     this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required]
+      permit_issued: [''],
+      permit_expiry: [''],
+      license_num: [''],
+      license_type_o: [''],
+      license_type_a: [''],
+      license_type_b: [''],
+      license_type_c: [''],
+      endorsement_h: [''],
+      endorsement_n: [''],
+      endorsement_p: [''],
+      endorsement_s: [''],
+      endorsement_t: [''],
+      endorsement_x: [''],
+      endorsement_none: [''],
+      restriction_e: [''],
+      restriction_k: [''],
+      restriction_l: [''],
+      restriction_m: [''],
+      restriction_n: [''],
+      restriction_o: [''],
+      restriction_p: [''],
+      restriction_v: [''],
+      restriction_x: [''],
+      restriction_z: [''],
+      restriction_none: ['']
     });
+    this.thirdFormGroup = this._formBuilder.group({});
   }
 }
